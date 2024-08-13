dd91d71f7239256644c0f218c81f42857f2ca471
Skythrew <mael.guerin@murena.io>
2024-08-13 19:31:22 +0200

    chore(project): release `0.1.0`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
