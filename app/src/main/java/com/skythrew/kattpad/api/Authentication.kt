70db5b12fe1e757a84aaf2773fd2c3f99b0311d8
Skythrew <mael.guerin@murena.io>
2024-08-15 13:44:54 +0200

    feat(Authentication): implement a simple `login` method

--- app/src/main/java/com/skythrew/kattpad/api/Authentication.kt
+++ app/src/main/java/com/skythrew/kattpad/api/Authentication.kt
@@ -1,8 +1,24 @@
 package com.skythrew.kattpad.api
 
+import io.ktor.http.parameters
+import io.ktor.http.setCookie
+
 open class Authentication (cookie: String) : com.skythrew.kattpad.api.config.Request(cookie) {
-    /**
-     * TODO
-     * Be able to log in and access profile infos
-     */
+    var loggedIn = false
+
+    suspend fun login(username: String, password: String) {
+        val loginResponse = this.post("https://www.wattpad.com/login", parameters {
+            append("username", username)
+            append("password", password)
+        })
+
+        val cookies = loginResponse.setCookie()
+
+        for (cookie in cookies) {
+            if (cookie.name == "token") {
+                loggedIn = true
+                break
+            }
+        }
+    }
 }
