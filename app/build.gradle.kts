c183cece361de7238f9aa6829ba40ff3896fd982
Skythrew <mael.guerin@murena.io>
2024-08-16 17:51:39 +0200

    chore(project): release 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.skythrew.kattpad"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
