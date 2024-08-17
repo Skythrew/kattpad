c825f69620e2cc30fd88b05cae30d050e2288f04
Skythrew <mael.guerin@murena.io>
2024-08-17 16:40:28 +0200

    feat(Comment): add ability to delete a comment

--- app/src/main/java/com/skythrew/kattpad/api/Comment.kt
+++ app/src/main/java/com/skythrew/kattpad/api/Comment.kt
@@ -2,13 +2,35 @@ package com.skythrew.kattpad.api
 
 import com.skythrew.kattpad.api.requests.CommentData
 import com.skythrew.kattpad.api.requests.CommentsResult
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
 
 class Comment (
     private val client: Wattpad,
     val data: CommentData
 ) {
+    suspend fun delete(): Boolean? {
+        if (!client.loggedIn)
+            return null
+
+        if (client.username != data.user.username)
+            return null
+
+        val res = client.makeRequest("v4", "comments/${data.commentId.resourceId}") {
+            method = HttpMethod.Delete
+        }
+
+        return res.status == HttpStatusCode.OK
+    }
+
     suspend fun fetchReplies(fields: Set<String> = setOf(), limit: Int = 0): List<Comment> {
-        val repliesResult = client.fetchObjData<CommentsResult>("v5", "comments/namespaces/comments/resources/${data.commentId.resourceId}/comments", fields, limit)
+        val repliesResult = client.fetchObjData<CommentsResult>(
+            api = "v5",
+            path = "comments/namespaces/comments/resources/${data.commentId.resourceId}/comments",
+            fields = fields,
+            limit = limit,
+            offset = 0
+        )
 
         return repliesResult.comments.map {data -> Comment(client, data) }
     }
