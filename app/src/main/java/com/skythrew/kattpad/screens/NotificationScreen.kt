312bd3ee7cda2cafb99645db33ef486f0a6340cd
Skythrew <mael.guerin@murena.io>
2024-08-27 19:53:52 +0200

    refactor: move `ProfilePicture` to a separate file

--- app/src/main/java/com/skythrew/kattpad/screens/NotificationScreen.kt
+++ app/src/main/java/com/skythrew/kattpad/screens/NotificationScreen.kt
@@ -11,11 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,7 +25,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -41,6 +38,7 @@ import coil.request.ImageRequest
 import com.skythrew.kattpad.R
 import com.skythrew.kattpad.api.Wattpad
 import com.skythrew.kattpad.api.requests.NotificationResponse
+import com.skythrew.kattpad.screens.utils.ProfilePicture
 import com.skythrew.kattpad.screens.utils.navigateOnce
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
@@ -225,23 +223,6 @@ fun NotificationScreen(padding: PaddingValues, navController: NavController, cli
                 }
             }
     }
-}
-
-@Composable
-fun ProfilePicture(url: String) {
-    AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(url)
-            .crossfade(true)
-            .build(),
-        contentDescription = stringResource(
-            id = R.string.profile_picture
-        ),
-        modifier = Modifier
-            .clip(CircleShape)
-            .width(50.dp)
-            .height(50.dp)
-    )
 }
 
 @Composable
---
+++ app/src/main/java/com/skythrew/kattpad/screens/utils/Pictures.kt
@@ -0,0 +1,31 @@
+package com.skythrew.kattpad.screens.utils
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.skythrew.kattpad.R
+
+@Composable
+fun ProfilePicture(url: String, modifier: Modifier = Modifier) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(url)
+            .crossfade(true)
+            .build(),
+        contentDescription = stringResource(
+            id = R.string.profile_picture
+        ),
+        modifier = modifier
+            .clip(CircleShape)
+            .width(50.dp)
+            .height(50.dp)
+    )
+}
