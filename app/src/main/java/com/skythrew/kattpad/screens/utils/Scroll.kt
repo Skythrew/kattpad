04e931e123c830557f9c5ba8b108c5b026ceeecc
Skythrew <mael.guerin@murena.io>
2024-08-28 16:44:41 +0200

    feat: add `reachedLast` lazyListState extension to utils

---
+++ app/src/main/java/com/skythrew/kattpad/screens/utils/Scroll.kt
@@ -0,0 +1,8 @@
+package com.skythrew.kattpad.screens.utils
+
+import androidx.compose.foundation.lazy.LazyListState
+
+internal fun LazyListState.reachedLast(buffer: Int = 1): Boolean {
+    val lastVisibleItem = this.layoutInfo.visibleItemsInfo.lastOrNull()
+    return lastVisibleItem?.index != 0 && lastVisibleItem?.index == this.layoutInfo.totalItemsCount - buffer
+}
