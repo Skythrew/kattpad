35181f7371d4db2bac2b8140db4e273eeb4109f1
Skythrew <mael.guerin@murena.io>
2024-08-28 17:14:39 +0200

    docs: add kdoc for `fetchStories`

--- app/src/main/java/com/skythrew/kattpad/api/WattpadList.kt
+++ app/src/main/java/com/skythrew/kattpad/api/WattpadList.kt
@@ -13,6 +13,15 @@ class WattpadList (
     private val client: Wattpad,
     val data: ListData
 ) {
+    /**
+     * Fetches the stories of the list represented by this class.
+     *
+     * @param fields Request fields to fetch (defaults to all fields)
+     * @param limit The maximum number of stories to fetch
+     * @param offset Request offset
+     *
+     * @return `ReadingListStoriesResponse` -> raw API response
+     */
     suspend fun fetchStories(fields: Set<String> = setOf(), limit: Int = 0, offset: Int = 0): ReadingListStoriesResponse {
         return client.fetchObjData<ReadingListStoriesResponse>(
             api = "v3",
