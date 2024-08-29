aeb8c42253fee596057209e29374c99af133aae9
Skythrew <mael.guerin@murena.io>
2024-08-29 10:30:22 +0200

    fix: `reachedLast` LazyListState extension implementation

--- app/src/main/java/com/skythrew/kattpad/screens/utils/Scroll.kt
+++ app/src/main/java/com/skythrew/kattpad/screens/utils/Scroll.kt
@@ -4,5 +4,9 @@ import androidx.compose.foundation.lazy.LazyListState
 
 internal fun LazyListState.reachedLast(buffer: Int = 1): Boolean {
     val lastVisibleItem = this.layoutInfo.visibleItemsInfo.lastOrNull()
-    return lastVisibleItem?.index != 0 && lastVisibleItem?.index == this.layoutInfo.totalItemsCount - buffer
+
+    return if (lastVisibleItem != null)
+        lastVisibleItem.index != 0 && lastVisibleItem.index >= this.layoutInfo.totalItemsCount - buffer
+    else
+        false
 }
